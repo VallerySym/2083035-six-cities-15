ac75b40f7a6c04884eef0798be90654176860e72
Валерия <justlera007@gmail.com>
2024-04-03 23:18:00 +0300

    Добавит тесты для favorites-process

--- src/store/favorites-process/favorites-process.selectors.ts
+++ src/store/favorites-process/favorites-process.selectors.ts
@@ -2,6 +2,9 @@ import { State } from '../../types/state';
 import { NameSpace } from '../../const';
 import { Offers } from '../../types/offers';
 
-export const getFavorites = (state: State): Offers => state[NameSpace.Favorites].favorites;
-export const getFavoritesIsLoading = (state: State): boolean => state[NameSpace.Favorites].favoritesIsLoading;
-export const getFavoritesIsNotFound = (state: State): boolean => state[NameSpace.Favorites].favoritesIsNotFound;
+export const getFavorites = (state: Pick<State, NameSpace.Favorites>): Offers =>
+    state[NameSpace.Favorites].favorites;
+export const getFavoritesIsLoading = (state: Pick<State, NameSpace.Favorites>): boolean =>
+    state[NameSpace.Favorites].favoritesIsLoading;
+export const getFavoritesIsNotFound = (state: Pick<State, NameSpace.Favorites>): boolean =>
+    state[NameSpace.Favorites].favoritesIsNotFound;
